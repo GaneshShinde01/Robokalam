9eda771bfbd4d13f3d82d621d5445a299afdac68
ganesh shinde <ganeshss978081@gmail.com>
2025-05-15 16:07:44 +0530

    working done

--- app/src/main/java/com/project/robokalam/Fragments/PortfolioFragment.java
+++ app/src/main/java/com/project/robokalam/Fragments/PortfolioFragment.java
@@ -28,7 +28,7 @@ public class PortfolioFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         //return inflater.inflate(R.layout.fragment_portfolio, container, false);
         binding = FragmentPortfolioBinding.inflate(inflater,container,false);
         View view = binding.getRoot();
